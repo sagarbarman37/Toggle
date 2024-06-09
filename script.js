3d5dab4773caff2dd4e8b22546c612c93343a06a
sagarbarman37 <167900058+sagarbarman37@users.noreply.github.com>
2024-06-09 21:13:42 +0530

    Update script.js
    Add local storage

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 let inputEl = document.querySelector(".input");
 let bodyEl = document.querySelector("body");
-// inputEl.checked = "false"
 
+inputEl.checked = JSON.parse(local storage.getItem('mode'));
+
+updateBody();
 function updateBody(){
   if(inputEl.checked){
     bodyEl.style.backgroundColor= "black"
   }else{
     bodyEl.style.backgroundColor= "white"
-  }
-}
+  };
+};
 
 inputEl.addEventListener("input", ()=>{
-  updateBody()
-})
+  updateBody();
+  updateLocalStorage();
+
+});
+
+function updateLocalStorage(){
+localStorage.setItem('mode',         JSON.stringify(inputEl.checked));
+};
